f9ee38ac2dd6c97645fa3e859addf1891c00a75f
Sarah Yang <sarahyang@Sarahs-MacBook-Pro-2.local>
2021-12-06 17:22:14 -0600

    added store/photos & store/albums

---
+++ frontend/src/store/album.js
@@ -0,0 +1,179 @@
+// import { useSelector } from "react-redux";
+import { csrfFetch } from "./csrf";
+
+
+const CREATE_ALBUM = "albums/CREATE_ALBUM";
+const EDIT_ALBUM = "albums/EDIT_ALBUM";
+const LOAD_ALBUMS = "albums/LOAD_ALBUMS";
+const LOAD_ONE_ALBUM = "albums/LOAD_ONE_ALBUM";
+const REMOVE_ALBUM = "albums/REMOVE_ALBUM";
+
+export const add = (newAlbum) => ({
+    type: CREATE_ALBUM,
+    newAlbum,
+});
+
+
+const edit = (album) => ({
+    type: EDIT_ALBUM,
+    album,
+});
+
+
+const loadAlbums = (album) => ({
+    type: LOAD_ALBUMS,
+    album,
+});
+
+const loadOneAlbum = (album) => ({
+    type: LOAD_ONE_ALBUM,
+    album,
+});
+
+const remove = (album) => ({
+    type: REMOVE_ALBUM,
+    album,
+});
+
+
+
+// add new album
+export const addNewAlbum = (album) => async (dispatch) => {
+    // const payload = useSelector((state) => state.payload || initialState);
+    // const { title, userID } = album;
+
+    // const data = {
+    //     albumTitle: title,
+    //     userID,
+    // };
+    // different from addPhoto
+
+    const res = await csrfFetch(`/api/albums`, {
+        method: "POST",
+        body: JSON.stringify(album),
+    });
+
+    if (res.ok) {
+        const newAlbum = await res.json();
+        dispatch(add(newAlbum));
+        return newAlbum;
+    }
+};
+
+
+//  edit / update
+export const editAlbum = (album) => async (dispatch) => {
+    const response = await fetch(`/api/albums/${album.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(album),
+    });
+
+    if (response.ok) {
+        const album = await response.json();
+        dispatch(edit(album));
+        return album;
+    }
+};
+
+
+// get more than one / get many 
+export const getAlbums = () => async (dispatch) => {
+    const res = await fetch("/api/albums");
+    // console.log(res, "<----res");
+
+    if (res.ok) {
+        const albums = await res.json();
+        // console.log(albums, "<------getAlbums albums");
+        dispatch(loadAlbums(albums));
+        return albums;
+    }
+};
+
+// console.log("HELLO");
+// get ONE album
+export const getOneAlbum = (albumID) => async (dispatch) => {
+    // console.log("tree lover ");
+    const res = await fetch(`/api/albums/${albumID}`);
+    // console.log("2");
+    if (res.ok) {
+        const album = await res.json();
+        dispatch(loadOneAlbum(album));
+        return album;
+    }
+};
+
+// delete  album / remove Album
+export const removeAlbum = (albumID) => async (dispatch) => {
+    const response = await fetch(`/api/albums/${albumID}`, {
+        method: "DELETE",
+    });
+
+    if (response.ok) {
+        const album = await response.json();
+        dispatch(remove(album.id));
+        return album;
+    }
+};
+
+
+
+const initialState = {};
+
+const albumReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case CREATE_ALBUM:
+            if (!state[action.newAlbum.id]) {
+                newState = { ...state, [action.newAlbum.id]: action.newAlbum };
+            }
+            return newState;
+            // newState = {};
+            // action.albums.forEach((album) => {
+            //     newState[album.id] = album;
+            // });
+            // return newState;
+
+        case EDIT_ALBUM:
+            if (!state[action.album.id]) {
+                newState = { ...state, [action.album.id]: action.album };
+                return newState;
+            } else {
+                return {
+                    ...state,
+                    [action.album.id]: {
+                        ...state[action.album.id],
+                        ...action.album,
+                    },
+                };
+            }
+
+        case LOAD_ALBUMS: {
+            newState = { ...state };
+
+            // console.log("fsdfaf--- ", action.album);
+            action.album?.forEach((album) => {
+                newState[action.album.id] = action.album;
+            });
+            return newState;
+        }
+
+        case LOAD_ONE_ALBUM:
+            newState = { ...state };
+            newState[action.album.id] = action.album;
+            return newState;
+
+        case REMOVE_ALBUM: {
+            newState = { ...state };
+            delete newState[action.albumID];
+            return newState;
+        }
+        default:
+            return initialState;
+    }
+};
+
+
+export default albumReducer;
---
+++ frontend/src/store/photos.js
@@ -0,0 +1,239 @@
+import { csrfFetch } from "./csrf";
+
+export const ADD_MORE_PHOTOS = "photos/ADD_MORE_PHOTOS";
+export const ADD_ONE_PHOTO = "photos/ADD_ONE_PHOTO";
+export const EDIT_PHOTO = "photos/EDIT_PHOTO";
+export const LOAD_MORE_PHOTOS = "photos/LOAD_PHOTOS";
+export const LOAD_ONE_PHOTO = "photos/LOAD_ONE_PHOTO";
+export const REMOVE_PHOTO = "photos/REMOVE_PHOTO";
+export const GET_ALL_PHOTOS = "photos/GET_ALL_PHOTOS";
+// action creator
+
+// add more than one photo option?
+const addMorePhotos = (photo) => {
+    return {
+        type: ADD_MORE_PHOTOS,
+        photo,
+    };
+};
+
+// this action object for adding a photo
+const add = (newPhoto) => {
+    return {
+        type: ADD_ONE_PHOTO,
+        newPhoto,
+    };
+};
+
+const edit = (photo) => {
+    return {
+        type: EDIT_PHOTO,
+        photo,
+    };
+};
+
+const getAllPhotos = (photos) => ({
+    type: GET_ALL_PHOTOS,
+    photos, // payload
+});
+
+const load = (photos) => ({
+    type: LOAD_MORE_PHOTOS,
+    photos,
+});
+
+const loadOnePhoto = (photo) => ({
+    type: LOAD_ONE_PHOTO,
+    photo,
+});
+
+const remove = (photo) => ({
+    type: REMOVE_PHOTO,
+    photo,
+});
+
+// add / create/ import photos
+export const addManyPhotos = () => async (dispatch) => {
+    const response = await csrfFetch("/api/photos");
+    if (response.ok) {
+        const photo = await response.json();
+        dispatch(addMorePhotos(photo));
+        return photo;
+    }
+};
+
+// this is a thunk to add photo
+export const addPhoto = (photo) => async (dispatch) => {
+    const response = await csrfFetch(`/api/photos`, {
+        method: "POST",
+        body: JSON.stringify(photo),
+    });
+    if (response.ok) {
+        const newPhoto = await response.json();
+        // console.log("This is new photo in Thunk", newPhoto);
+        dispatch(add(newPhoto));
+        return newPhoto;
+    }
+};
+
+//  edit / update
+export const editPhoto = (photoID, post) => async (dispatch) => {
+    const { albumID, content, userID } = post;
+
+    const response = await csrfFetch(`/api/photos/${photoID}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            albumID,
+            content,
+            userID,
+            imageID: photoID,
+        })
+    });
+
+    if (response.ok) {
+        const photo = await response.json();
+        dispatch(edit(photo));
+        return photo;
+    }
+};
+// `/api/photos/${photo.id}`
+
+//  edit / update
+// export const editPhoto = (photo) => async (dispatch) => {
+//     const response = await fetch(`/api/photos/${photo.id}`, {
+//         method: "PUT",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(photo),
+//     });
+
+//     if (response.ok) {
+//         const photo = await response.json();
+//         dispatch(edit(photo));
+//         return photo;
+//     }
+// };
+
+export const getAllThePhotos = () => async (dispatch) => {
+    const response = await fetch(`/api/photos`);
+
+    if (response.ok) {
+        const photos = await response.json();
+        dispatch(getAllPhotos(photos));
+        return photos;
+    }
+};
+
+// get more than one
+export const getPhotos = () => async (dispatch) => {
+    const response = await fetch(`/api/photos`);
+
+    if (response.ok) {
+        const photos = await response.json();
+        dispatch(load(photos));
+        return photos;
+    }
+};
+
+// get ONE photo
+export const getOnePhoto = (photoID) => async (dispatch) => {
+    const response = await fetch(`/api/photos/${photoID}`);
+
+    if (response.ok) {
+        const photo = await response.json();
+        // console.log(photos, '<------getPhotos photos')
+        dispatch(loadOnePhoto(photo));
+        return photo;
+    }
+};
+
+// delete / remove
+export const removePhoto = (photoID) => async (dispatch) => {
+
+    const response = await csrfFetch(`/api/photos/${photoID}`, {
+        method: "DELETE",
+    });
+    if (response.ok) {
+        const photo = await response.json();
+        dispatch(remove(photo));
+        // console.log("this is delete route", response);
+        // dispatch(remove(photoID));
+        return photo;
+    }
+};
+
+const initialState = {};
+
+// Reducer --- changes data in my store
+const photosReducer = (state = initialState, action) => {
+    let newState; // or....  let newState = {};
+    switch (action.type) {
+        case ADD_MORE_PHOTOS:
+            action.photo.forEach((photo) => (newState[photo.id] = photo));
+            return newState;
+
+        case ADD_ONE_PHOTO:
+            if (!state[action.newPhoto.id]) {
+                newState = { ...state, [action.newPhoto.id]: action.newPhoto };
+            }
+            return newState;
+
+        case EDIT_PHOTO:
+            if (!state[action.photo.id]) {
+                newState = { ...state, [action.photo.id]: action.photo };
+                return newState;
+            } else {
+                return {
+                    ...state,
+                    [action.photo.id]: {
+                        ...state[action.photo.id],
+                        ...action.photo,
+                    },
+                };
+            }
+
+        case GET_ALL_PHOTOS: {
+            // let photoObj = Object.assign({}, action.photos)
+            const newState = {  };
+            action.photos.forEach((photo) => {
+                newState[photo.id] = action.photo;
+            });
+            return newState;
+        }
+
+        case LOAD_MORE_PHOTOS: {
+            newState = { ...state };
+            // console.log("fsdfaf--- ", action.photos);
+            action.photos?.forEach((photo) => {
+                newState[action.photos.id] = action.photos;
+            });
+            return newState;
+        }
+
+        case LOAD_ONE_PHOTO:
+            newState = { ...state };
+            newState[action.photo.id] = action.photo;
+            return newState;
+
+        case REMOVE_PHOTO: {
+            newState = { ...state };
+            // console.log("tree lover", action.photo);
+            delete newState[+action.photo.photoID]; // from res.json({photoID})
+            // photoID ---- key inside action.photo. 
+            // action.photo is the object we sent in
+
+            return newState;
+        }
+
+        default:
+            return state;
+    }
+};
+
+export default photosReducer;
+
+//? Note ------ used pokedex for reference
